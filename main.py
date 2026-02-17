36c03b2e9e3e2e01465ae43053cbeba6db18f6e9
alinazvorygina63-code <alinazvorygina63@gmail.com>
2026-02-17 13:55:25 +0300

    loop.py

--- main.py
+++ main.py
@@ -8,3 +8,11 @@ def generate_password():
         length = int(entry.get())
     except ValueError:
         return
+     for i in range(length):
+        if random.random() > 0.5:
+            password += str(random.randint(0, 9))
+        else:
+            if random.random() > 0.5:
+                password += chr(random.randint(97,122))
+            else:
+                password += chr(random.randint(65, 90))
